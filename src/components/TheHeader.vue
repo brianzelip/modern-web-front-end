98e02164de6fb3969e66b58b40e8ca36c064877c
Brian Zelip <bzelip@gmail.com>
2019-05-31 15:55:32 -0400

    TheHeader: Flesh out list and styles

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,22 +1,37 @@
 <template lang="pug">
-  header.flex.justify-space-between.container
+  header.flex.justify-space-between.container.py2
     h1.h3.m0.py2.yellow Star Wars data
     ul.mb0.list-reset.flex.flex-center.yellow
-      li people
-      li films
-      li starships
-      li vehicles
-      li species
-      li planets
+      li
+        button.btn.regular films
+      li
+        button.btn.regular people
+      li
+        button.btn.regular planets
+      li
+        button.btn.regular species
+      li
+        button.btn.regular starships
+      li
+        button.btn.regular vehicles
 </template>
 
 <style scoped>
-ul li {
+li {
   margin-right: 0.5rem;
+}
+
+li:last-child {
+  margin-right: 0;
+}
+
+.btn {
   padding: 1rem;
 }
-ul li:hover {
-  background-color: rgba(255, 255, 255, 0.5);
+
+.btn:hover {
+  background-color: rgba(255, 255, 255, 0.3);
+  cursor: default;
 }
 </style>
 
