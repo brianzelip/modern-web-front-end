ff8ba45bbf678a7f6296857769eaa3eb4a274e5d
Brian Zelip <bzelip@gmail.com>
2019-05-31 12:16:54 -0400

    TheHeader: Add list markup

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,3 +1,22 @@
 <template lang="pug">
-  h1.yellow.center Modern Web Workshop Front End
+  header.flex.justify-space-between.container
+    h1.h3.m0.py2.yellow Star Wars data
+    ul.mb0.list-reset.flex.flex-center.yellow
+      li people
+      li films
+      li starships
+      li vehicles
+      li species
+      li planets
 </template>
+
+<style scoped>
+ul li {
+  margin-right: 0.5rem;
+  padding: 1rem;
+}
+ul li:hover {
+  background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
+
