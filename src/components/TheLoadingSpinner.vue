98fea665ecbc75f72e936e2e849f452fb5324d6f
Brian Zelip <bzelip@gmail.com>
2019-06-04 13:56:34 -0400

    TheLoadingSpinner: Update template and styles

--- src/components/TheLoadingSpinner.vue
+++ src/components/TheLoadingSpinner.vue
@@ -1,21 +1,26 @@
 <template lang="pug">
   section
       div.loader
+      p.yellow Fetching data
 </template>
 
 <style>
 /* via https://www.w3schools.com/howto/howto_css_loader.asp */
 .loader {
-  border: 16px solid #f3f3f3; /* Light grey */
-  border-top: 16px solid #cb8e14;
+  border: 10px solid rgba(255, 255, 255, 0.4); /* Light grey */
+  border-top: 10px solid #cb8e14;
   border-radius: 50%;
-  width: 120px;
-  height: 120px;
-  animation: spin 2s linear infinite;
+  width: 75px;
+  height: 75px;
+  animation: spin 1.5s linear infinite;
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
