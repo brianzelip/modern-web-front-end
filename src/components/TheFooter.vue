93b31d412a7f77f30d2628db07f200bdd8126239
Brian Zelip <bzelip@gmail.com>
2019-06-05 06:57:20 -0400

    TheFooter: Add html entities for white space to get around display:flex removal of inline white space

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
   footer
     a.white.yellow-hover(href="https://zelip.me") by bzelip
-    | 
-    span.yellow |
-    | 
+    |
+    span.block.yellow &nbsp; | &nbsp;
+    |
     a.white.yellow-hover(href="https://github.com/brianzelip/modern-web-front-end") source code
-    | 
-    span.yellow |
+    |
+    span.yellow &nbsp; | &nbsp;
     | 
     a.white.yellow-hover(href="http://modern-web-back-end.glitch.me/") api
-    | 
-    span.yellow |
-    | 
+    |
+    span.yellow &nbsp; | &nbsp;
+    |
     a.white.yellow-hover(href="https://github.com/brianzelip/modern-web-back-end") api source code
 </template>
