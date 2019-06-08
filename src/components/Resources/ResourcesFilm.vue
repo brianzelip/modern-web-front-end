54d2246fb117746465f8681327e34dbadc566533
Brian Zelip <bzelip@gmail.com>
2019-06-08 08:20:47 -0400

    ResourcesFilm: Copy markup from TheDataGrid as pug

--- src/components/Resources/ResourcesFilm.vue
+++ src/components/Resources/ResourcesFilm.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
-  
+  article.flex.flex-column.px2
+    h1 entry.title
+    figure
+      img(alt="entry.imageDescription" entry.imgUrl)
+      caption entry.imageCaption
+    p Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ut iure quisquam, exercitationem et dignissimos distinctio porro sint pariatur inventore, enim voluptate nobis expedita voluptatum culpa illum reiciendis error eum.
 </template>
 
 <script>
