a89439da44720bd1d55402dc22ed96eef55c6217
Brian Zelip <bzelip@gmail.com>
2019-06-08 11:44:04 -0400

    ResourcesPerson.vue: Render people data via props and markup

--- src/components/Resources/ResourcesPerson.vue
+++ src/components/Resources/ResourcesPerson.vue
@@ -1,9 +1,33 @@
 <template lang="pug">
-  
+  article.flex.flex-column.px2
+    h1 {{ data.name }}
+    p
+      span.bold Species: 
+      | {{ data.species }}    
+    p
+      span.bold Birth planet: 
+      | {{ data.homeworld }}
+    p
+      span.bold Birth year: 
+      | {{ data.birth_year }}
+    p
+      span.bold Films: 
+      ul
+        li(v-for="(film, index) in data.films" :key="index") {{ film }}
+    //- figure
+    //-   img(alt="entry.imageDescription" entry.imgUrl)
+    //-   caption entry.imageCaption
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["data"],
+  data() {
+    return {
+      people: ["name", "birth_year", "homeworld", "films", "species"]
+    };
+  }
+};
 </script>
 
 <style scoped>
