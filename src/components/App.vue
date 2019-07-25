4b13562731b494170dd429eeb101da65f8d1916c
Brian Zelip <bzelip@gmail.com>
2019-07-25 09:46:48 -0400

    App: Ready this file for initial use

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,72 +1,28 @@
-<template lang="pug">
-  div#app.vh100
-    TheHeader(v-on:resource-update="getData")
-
-    TheUserInstructions.flex-grow.flex.justify-content-center.align-items-center(v-if="showInstructions")
-
-    //- TheStarWarsCrawl.flex-grow.container(v-if="showInstructions")
-
-    TheLoadingSpinner.flex-grow.flex.flex-column.justify-content-center.align-items-center(v-if="showSpinner")
-
-    TheDataGrid(:resource="resource" :swapiData="swapiPayload.results" v-if="showDataGrid")
-
-    TheFooter.flex.justify-content-center.align-items-end.pb2
+<template>
+  <main>
+  
+    <p>{{ message }}</p>
+    <input v-model="message">
+    
+    <!-- <hr>
+    <p>Reversed message: {{ reversedMessage }}</p> -->
+  
+  </main>
 </template>
 
 <script>
-import axios from "axios";
-
-import TheHeader from "./TheHeader.vue";
-import TheStarWarsCrawl from "./TheStarWarsCrawl.vue";
-import TheUserInstructions from "./TheUserInstructions.vue";
-import TheLoadingSpinner from "./TheLoadingSpinner.vue";
-import TheDataGrid from "./TheDataGrid.vue";
-import TheFooter from "./TheFooter.vue";
 
 export default {
   data() {
     return {
-      url: "https://modern-web-back-end.glitch.me/get/",
-      resource: "",
-      swapiPayload: {},
-      showInstructions: true,
-      showSpinner: false,
-      showDataGrid: false
+      message: 'Hello world!'
     };
-  },
-  components: {
-    TheHeader,
-    TheStarWarsCrawl,
-    TheUserInstructions,
-    TheLoadingSpinner,
-    TheDataGrid,
-    TheFooter
-  },
-  methods: {
-    getData(resource) {
-      this.$set(this, "showInstructions", false);
-      this.$set(this, "showDataGrid", false);
-      this.$set(this, "showSpinner", true);
-
-      axios
-        .get(`${this.url}${resource}`)
-        .then(response => {
-          this.$set(this, "showSpinner", false);
-          this.$set(this, "showDataGrid", true);
-          this.$set(this, "resource", resource);
-          this.$set(this, "swapiPayload", response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
   }
+  // ,
+  // computed: {
+  //   reversedMessage: function () {
+  //     return this.message.split('').reverse().join('')
+  //   }
+  // }
 };
 </script>
-
-<style scoped>
-#app {
-  display: flex;
-  flex-direction: column;
-}
-</style>
